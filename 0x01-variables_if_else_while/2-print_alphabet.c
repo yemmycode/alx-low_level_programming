be8ec28211071b62b1fd0fa837071f2a83dc16f1
yemmycode <yemiajibade17@gmail.com>
2023-07-14 11:41:12 -0700

    Lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int lowerCase = 'a';
+	while (lowerCase <= 'z')
+
+	{putchar(lowerCase);
+		lowerCase++;}
+
+		putchar('\n');
+		return (0);
+}
+
