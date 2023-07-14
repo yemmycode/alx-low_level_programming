695bdeadb25d0854840d475b21ec94fb38cd5715
yemmycode <yemiajibade17@gmail.com>
2023-07-13 20:06:55 -0700

    programming a multilingual language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main: point of entry
- * return: 0
+ * main:point of entry
+ * return:0
  */
-int main ()
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+    return(0);
 }
