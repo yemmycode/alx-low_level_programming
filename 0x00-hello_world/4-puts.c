d69edf5811f93025f991bcea215e3f65c737b3fb
yemmycode <yemiajibade17@gmail.com>
2023-07-13 20:14:56 -0700

    programming languge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
- * main:point of entry
- * return:0
+/**
+ * main: point of entry
+ *
+ * return: 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+    puts ("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
