72760f5c39e749099525d4a3893c1c77bc2a59ee
yemmycode <yemiajibade17@gmail.com>
2023-07-14 19:56:46 -0700

    Patience for success

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all possible different combinations of three digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
+	
+	for(hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) ||
