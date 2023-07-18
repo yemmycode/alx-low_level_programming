084c1de8522c39652cd01fce8da7f0284894a497
yemmycode <yemiajibade17@gmail.com>
2023-07-18 14:49:11 -0700

    Add untracked files

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int lowerCase = 'a';
+	while
+		(lowerCase <= 'z')
+		{
+			if
+				(lowerCase == 'e' || lowerCase == 'q')
+
+		}
+	lowerC  clear
+
