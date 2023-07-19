163b4e7067f78d9b75634d38cbdd68c39cd0e369
yemmycode <yemiajibade17@gmail.com>
2023-07-19 00:17:35 -0700

    natural number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,22 +2,22 @@
 
 /**
  * main - prints the sum of all multiples of 3 or 5 up to 1024
- * 
+ *
  * Return: 0
  */
 
 int main(void)
 {
-	int i, w = 0;
+        int i, w = 0;
 
-	while (i < 1024)
-	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			w += i;
-		}
-		i++;
-	}
-	printf("%d\n", w);
-	return (0);
+        while (i < 1024)
+        {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                        w += i;
+                }
+                i++;
+        }
+        printf("%d\n", w);
+        return (0);
 }
