863b52cdc074c0dcf856d5185d18cf273305f59e
yemmycode <yemiajibade17@gmail.com>
2023-07-18 23:56:25 -0700

    Natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,17 +6,17 @@
  * Return: 0
  */
 
-int main() {
-    int lim = 1024;
-    int sum = 0;
+int main(void)
+{
+    int i, w = 0;
 
-    for (int i = 1; i < lim; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
+    while (i < 1024)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            w += i;
         }
+        i++;
     }
-
-    printf("sum of multiples of 3 or 5 below %d is: %d\n", lim, sum);
-
-    return 0;
-}
+    printf("%d\n", w);
+    return (0);
