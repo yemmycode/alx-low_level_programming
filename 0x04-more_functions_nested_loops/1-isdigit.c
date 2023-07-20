b97144692e2c1e3e8e7abc451d31eda0b2c1ae89
yemmycode <yemiajibade17@gmail.com>
2023-07-20 10:30:16 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,11 @@
  * Return: 1 for digit character or 0 for anything else
  */
 
-int _isdigit(int c);
+int _isdigit(int c){
 
-{
 if (c >= 48 && c <= 57)
 {
-return(1);
+return (1);
 }
 return (0);
 }
