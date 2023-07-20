4d17d60e2a138f31efd0370119564f323c0632e7
yemmycode <yemiajibade17@gmail.com>
2023-07-19 17:45:09 -0700

    fibonacci fifty series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,23 +8,23 @@
 
 int main(void)
 {
-        int i = 0;
-        long f = 1, g = 2;
-
-        while (i < 50)
-        {
-                if (i == 0)
-                        printf("%ld", f);
-                else if (i == 1)
-                        printf(", %ld", g);
-                else
-                {
-                        g += f;
-                        f = g - f;
-                        printf(", %ld", g);
-                }
-                ++i;
-        }
-        printf("\n");
-        return (0);
-} 
+	int i = 0;
+	long f = 1, g = 2;
+	
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", f);
+		else if (i == 1)
+			printf(", %ld", g);
+		else
+		{
+			g += f;
+			f = g - f;
+			printf(", %ld", g);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
