21ac734e017c8913827a7a59d845da630417140d
yemmycode <yemiajibade17@gmail.com>
2023-07-19 18:51:27 -0700

    fibonacci 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
-
-	while (k + j < 4000000)
+	int a = 0;
+	long b = 1, c = 2, sum = c;
+        
+	while (c + b < 4000000)
 	{
-		k += j;
-		if (k % 2 == 0)
-			sum += k;
-		j = k - j;
-		++i;
+		c += b;
+		if (c % 2 == 0)
+			sum += c;
+		b = c - b;
+		++a;
 	}
 	printf("%ld\n", sum);
 	return (0);
