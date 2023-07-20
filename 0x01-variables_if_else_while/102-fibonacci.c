f8c373aa57fb2b49f0f33ba4c06fc9be393b3a15
yemmycode <yemiajibade17@gmail.com>
2023-07-19 17:55:50 -0700

    fibonacci fifty series

---
+++ 0x01-variables_if_else_while/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+        int i = 0;
+        long f = 1, g = 2;
+	
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", f);
+		else if (i == 1)
+			printf(", %ld", g);
+		else
+		{
+			g += f;
+			f = g - f;
+			printf(", %ld", g);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
