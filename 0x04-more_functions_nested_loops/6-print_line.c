5d57357e4c1e2b276a0e482299fd58b013efe076
yemmycode <yemiajibade17@gmail.com>
2023-07-20 15:58:54 -0700

    six print

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line -prints a straight line
+ * @n: Number of times to print
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+	
+	while (i < n)
+	{
+		_putchar('_');
+		
+		i++;
+	}
+	_putchar('\n');
+}
