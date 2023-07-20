975c8d487af65a684576ef166a6cd3486406446a
yemmycode <yemiajibade17@gmail.com>
2023-07-19 19:51:05 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,46 +2,55 @@
 
 /**
  * main - prints the very first 98 fibonacci numbers 
- * starting with 1 & 2, seperated by comma and followed 
- * by a space
+ * starting with 1 & 2, seperated by comma, followed
+ * by space.
  * Return: 0
  */
+#include <stdio.h>
+
 int main(void)
 {
-int count;
-unsigned long fib1 = 0, fib2 = 1, sum;
-unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-unsigned long half1, half2;
+    int count;
+    unsigned long long fib1 = 0, fib2 = 1, sum;
+    unsigned long long fib1_upper, fib2_upper, fib1_lower, fib2_lower;
+    unsigned long long upper, lower;
 
-for (count = 0; count < 92; count++)
-{
-sum = fib1 + fib2;
-printf("%lu, ", sum);
-fib1 = fib2;
-fib2 = sum;
-}
-fib1_half1 = fib1 / 10000000000;
-fib2_half1 = fib2 / 10000000000;
-fib1_half2 = fib1 % 10000000000;
-fib2_half2 = fib2 % 10000000000;
-for (count = 93; count < 99; count++)
-{
-half1 = fib1_half1 + fib2_half1;
-half2 = fib1_half2 + fib2_half2;
-if (fib1_half2 + fib2_half2 > 9999999999)
-{
-half1 += 1;
-half2 %= 10000000000;
-}
-printf("%lu%lu", half1, half2);
-if (count != 98)
-printf(", ");
-fib1_half1 = fib2_half1;
-fib1_half2 = fib2_half2;
-fib2_half1 = half1;
-fib2_half2 = half2;
-}
-print ("\n");
-return (0);
+    for (count = 0; count < 92; count++)
+    {
+        sum = fib1 + fib2;
+        printf("%llu, ", sum);
+        fib1 = fib2;
+        fib2 = sum;
+    }
+
+    fib1_upper = fib1 / 10000000000;
+    fib2_upper = fib2 / 10000000000;
+    fib1_lower = fib1 % 10000000000;
+    fib2_lower = fib2 % 10000000000;
+
+    for (count = 93; count < 100; count++)
+    {
+        upper = fib1_upper + fib2_upper;
+        lower = fib1_lower + fib2_lower;
+
+        if (fib1_lower + fib2_lower > 9999999999)
+        {
+            upper += 1;
+            lower %= 10000000000;
+        }
+
+        printf("%llu%010llu", upper, lower);
+
+        if (count != 99)
+            printf(", ");
+
+        fib1_upper = fib2_upper;
+        fib1_lower = fib2_lower;
+        fib2_upper = upper;
+        fib2_lower = lower;
+    }
+
+    printf("\n");
+    return 0;
 }
 
