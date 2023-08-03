5dda72fade1ec90656bc4264e71bd89c9b6f9619
yemmycode <yemiajibade17@gmail.com>
2023-08-02 19:35:21 -0700

    She locked away a secret

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,17 @@
 
 /**
  * _puts_recursion - This prints a string followed by a new line
- * @s: String to print
+ * @s: string to print
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-_putchar(*s);
 if (*s == '\0')
-_puts_recursion(s + 1);
-else
+{
 _putchar('\n');
+return;
+}
+
+_putchar(*s);
+_puts_recursion(s + 1);
 }
