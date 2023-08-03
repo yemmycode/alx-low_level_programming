72b949fb016b69fbedf879e0ea076912f642a7c5
yemmycode <yemiajibade17@gmail.com>
2023-08-02 21:45:04 -0700

    They say we only use a fraction of our brain's true potential

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,7 @@
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-return 0;
+return (0);
 return 1 + _strlen_recursion(s + 1);
 }
 
@@ -24,10 +24,10 @@ int comparator(char *s, int n1, int n2)
 if (*(s + n1) == *(s + n2))
 {
 if (n1 >= n2)
-return 1;
+return (1);
 return comparator(s, n1 + 1, n2 - 1);
 }
-return 0;
+return (0);
 }
 
 /**
@@ -38,7 +38,7 @@ return 0;
 int is_palindrome(char *s)
 {
 if (*s == '\0')
-return 1;
+return (1);
 return comparator(s, 0, _strlen_recursion(s) - 1);
 }
 
