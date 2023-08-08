0d7b68aa0d170c28fe937c6bc77e2f3e72f21c1f
yemmycode <yemiajibade17@gmail.com>
2023-08-08 02:34:41 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strdup - this copies the string to new space in memory
- * @str: the string to copy into new memory
- * Return: the pointer to the replicated memory
- */
+* _strdup - this copies the string to new space in memory
+* @str: the string to copy into new memory
+* Return: the pointer to the replicated memory
+*/
 
 char *_strdup(char *str)
 {
@@ -20,7 +20,7 @@ count++;
 
 repl = (char *)malloc((count + 1) * sizeof(char));
 
-if (repl == NULL)	
+if (repl == NULL)
 return (NULL);
 for (i = 0; str[i] != '\0'; i++)
 repl[i] = str[i];
