af94a6d1f092130f41f58ff51a110f4f72bf8eeb
yemmycode <yemiajibade17@gmail.com>
2023-08-15 11:05:15 -0700

    task two

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,17 +5,12 @@
  * main - this prints the file name
  * Return: 0
  */
+
 int main(void)
 {
 char *filename = (char *)__FILE__; 
 
-FILE *file = fopen("output.txt", "w"); 
-
-if (file != NULL) 
-{
-fputs(filename, file); 
-fclose(file); 
-}
+printf("%s\n", filename);
 
 free(filename);
 
