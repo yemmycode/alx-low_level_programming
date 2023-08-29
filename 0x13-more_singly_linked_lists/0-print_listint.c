6ea96e62a7513cf457f2109d601e6a39910351a2
yemmycode <yemiajibade17@gmail.com>
2023-08-29 03:35:31 -0700

    task zero

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_listint - Display all elements contained
+* within a listint_t linked list.
+* @h: The head node of the linked list.
+* Return: The count of nodes present in the list.
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t nodeCount;
+nodeCount = 0;
+
+while (h != NULL)
+{
+printf("Value: %d\n", h->n);
+h = h->next;
+nodeCount++;
+}
+
+return (nodeCount);
+}
