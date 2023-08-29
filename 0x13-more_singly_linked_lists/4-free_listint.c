e40e69e8808e382f364ea8d17cce5ec946b447dc
yemmycode <yemiajibade17@gmail.com>
2023-08-29 08:25:46 -0700

    task four

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+* free_listint - Frees a linked list of integers.
+* @head: Pointer to the head node of the linked list.
+*
+* This function frees each node in a linked list of integers.
+*/
 void free_listint(listint_t *head)
 {
 while (head != NULL)
