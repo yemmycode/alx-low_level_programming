abc500889925be223b9b8f2b48bc3d36971639ea
yemmycode <yemiajibade17@gmail.com>
2023-12-18 17:50:22 -0800

    The file io project

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,118 +1,96 @@
-/* 
- * This program reads an ELF file and prints its header information
- * Functions: 
- * print_elf_magic: prints the magic bytes of the ELF header
- * print_elf_class: prints the class of the ELF file (32-bit or 64-bit)
- * print_elf_data: prints the data encoding of the ELF file (little endian or big endian)
- * print_elf_header: prints the version, OS/ABI, ABI version, type, and entry point of the ELF file
- * open_elf_file: opens an ELF file and returns a file descriptor
- * main: takes an ELF file name as an argument and calls the other functions
- */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <elf.h>
-#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
 
-#define MAX_SIZE 1000
-#define MAGIC "\x7fELF"
+#define ELFMAG0 0x7f
+#define ELFMAG1 'E'
+#define ELFMAG2 'L'
+#define ELFMAG3 'F'
 
-void print_elf_magic(const Elf64_Ehdr *elf_header) 
-{
-printf("Magic:   ");
-for (int i = 0; i < EI_NIDENT; i++) 
-{
-printf("%02x ", elf_header->e_ident[i]);
-}
-printf("\n");
-}
+void print_elf_header_info(Elf64_Ehdr *header);
+char *get_class(unsigned char e_ident[]);
+char *get_data(unsigned char e_ident[]);
 
-void print_elf_class(const Elf64_Ehdr *elf_header) 
+int main(int argc, char *argv[])
 {
-printf("Class:   ");
-switch (elf_header->e_ident[EI_CLASS]) 
-{
-case ELFCLASS32:
-printf("ELF32\n");
-break;
-case ELFCLASS64:
-printf("ELF64\n");
-break;
-default:
-printf("Unknown\n");
-break;
-}
-}
+int fd;
+Elf64_Ehdr header;
 
-void print_elf_data(const Elf64_Ehdr *elf_header) 
-{
-printf("Data:    ");
-switch (elf_header->e_ident[EI_DATA]) 
-{
-case ELFDATA2LSB:
-printf("2's complement, little endian\n");
-break;
-case ELFDATA2MSB:
-printf("2's complement, big endian\n");
-break;
-default:
-printf("Unknown\n");
-break;
-}
-}
-
-void print_elf_header(const Elf64_Ehdr *elf_header) 
-{
-print_elf_magic(elf_header);
-print_elf_class(elf_header);
-print_elf_data(elf_header);
-printf("Version: %d (current)\n", elf_header->e_ident[EI_VERSION]);
-printf("OS/ABI:  %d\n", elf_header->e_ident[EI_OSABI]);
-printf("ABI Version: %d\n", elf_header->e_ident[EI_ABIVERSION]);
-printf("Type:    %d\n", elf_header->e_type);
-printf("Entry point address: %lx\n", (unsigned long)elf_header->e_entry);
-}
-
-int open_elf_file(const char *filename) 
-{
-int fd = open(filename, O_RDONLY);
-if (fd == -1) {
-perror("Error opening file");
-exit(98);
-}
-return (fd);
-}
-
-int main(int argc, char *argv[]) 
-{
-if (argc != 2) 
+if (argc != 2)
 {
 fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
 exit(98);
 }
 
-const char *filename = argv[1];
-int fd = open_elf_file(filename);
-
-Elf64_Ehdr elf_header;
-if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header)) 
+fd = open(argv[1], O_RDONLY);
+if (fd == -1)
 {
-perror("Error reading ELF header");
+perror("Error");
+exit(98);
+}
+
+if (sizeof(header) != read(fd, &header, sizeof(header)))
+{
+perror("Error");
 close(fd);
 exit(98);
 }
 
-if (memcmp(elf_header.e_ident, MAGIC, 4) != 0) 
+if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+header.e_ident[EI_MAG1] != ELFMAG1 ||
+header.e_ident[EI_MAG2] != ELFMAG2 ||
+header.e_ident[EI_MAG3] != ELFMAG3)
 {
 fprintf(stderr, "Error: Not an ELF file\n");
 close(fd);
 exit(98);
 }
 
-print_elf_header(&elf_header);
+print_elf_header_info(&header);
 
 close(fd);
+
 return (0);
+}
+
+void print_elf_header_info(Elf64_Ehdr *header)
+{
+int i;
+
+printf("Magic: ");
+for (i = 0; i < EI_NIDENT; i++)
+printf("%02x ", header->e_ident[i]);
+printf("\n");
+
+printf("Class: %s\n", get_class(header->e_ident));
+
+printf("Data: %s\n", get_data(header->e_ident));
+
+printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
+
+printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
+
+printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+
+printf("Type: %d\n", header->e_type);
+
+printf("Entry point address: 0x%lx\n", header->e_entry);
+}
+
+char *get_class(unsigned char e_ident[])
+{
+switch (e_ident[EI_CLASS])
+{
+case ELFCLASSNONE:
+return ("none");
+case ELFCLASS32:
+return ("ELF32");
+case ELFCLASS64:
+return ("ELF64");
+default:
+return ("<unknown>");
+}
 }
